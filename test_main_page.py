703e9a48ed477a0a981d2706eb5d868e8150fe72
TischenkoOleg <tischenkooleg87@gmail.com>
2023-04-21 16:04:46 +0300

    modification

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,16 @@
 import time
+import pytest
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 link = "http://selenium1py.pythonanywhere.com/"
 def test_guest_can_go_to_login_page(browser):
     browser.get(link)
+    go_to_login_page(browser)
+
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
-    time.sleep(10)
+    time.sleep(5)
+    
+#pytest --browser_name=chrome --language=en test_main_page.py
