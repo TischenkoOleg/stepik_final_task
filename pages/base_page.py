687a34bfc610ed75bdd8255f24b77da0d392abfa
TischenkoOleg <tischenkooleg87@gmail.com>
2023-04-21 20:44:38 +0300

    Checking an element on a page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,25 +1,26 @@
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        
+        self.browser.implicitly_wait(timeout)
     def open(self):
         self.browser.get(self.url)
 
-   # def is_element_present(self, how, what):
-    #    try:
-     #       self.browser.find_element(how, what)
-     #   except (NoSuchElementException):
-      #      return False
-      #  return True
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
 
    # def go_to_login_page(self):
   #      link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
   #      link.click()
 
-  #  def should_be_login_link(self):
-  #      assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+    #def should_be_login_link(self):
+     #   assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
 
    # def open_basket(self):
     #    basket_btn = self.browser.find_element(*BasePageLocators.BASKET_LOCATOR)
--- pages/test_main_page.py
+++ pages/test_main_page.py
@@ -1,13 +1,22 @@
 import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from pages.main_page import MainPage
-
+from pages.base_page import BasePage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
     page.open()                    # открываем страницу
-    page.go_to_login_page()        # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()# выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
     time.sleep(5)
     
 #pytest --browser_name=chrome --language=en test_main_page.py
 #pytest -v --tb=line --language=en test_main_page.py
+
